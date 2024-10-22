19134345555844720f9a3309ea766b0fb6231cd4
Anthony Gulyk <agulyk82@gmail.com>
2024-10-22 17:10:20 -0500

    Added logic to create random arrays

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface Props {
+    arr: Array<number>;
+    arrSize: number;
+};
+
+const ArrayBars: React.FC<Props> =({arr, arrSize}) => (
+    <div className='bars'>
+        {arr.map((value: number, idx: number) => (
+            <div
+                className='array-bar'
+                key={idx}
+                style={{ backgroundColor: 'navy', height: `${value}px`, width: arrSize === 20 ? '2%' : '.5%' }}
+                >
+                </div>
+        ))}
+        </div>
+);
+
+const generateArray = (arrSize: number) => Array.from({ length: arrSize }, () => Math.floor(Math.random() * (100 - 5 + 1) * 5));
+
+
+const App = () => {
+    const [arr, setArr] = useState<Array<number>>(generateArray(20));
+    const [arrSize, setArrSize] = useState<number>(20);
+
+    const newArray = () => setArr(generateArray(arrSize));
+
+    const toggleSize = () => {
+        setArrSize(arrSize === 20 ? 120 : 20 );
+        setSpeeds(arrSize === 20 ? speedsFast : speedsSlow);
+        setArr(generateArray(arrSize === 20 ? 120 : 20));
+    };
+
+};
+
 
 export default App;
